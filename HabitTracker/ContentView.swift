5788c47a6942f01c3b63868ef89340bca9501301
jyhaoo <jyhaoo1998@gmail.com>
2025-09-19 20:48:41 -0700

    chore: learning content view for swift

--- HabitTracker/ContentView.swift
+++ HabitTracker/ContentView.swift
@@ -9,16 +9,27 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
-    @State private var habits = ["Drink Water", "Exercise", "Read"]
+    // @State private var habits = ["Drink Water", "Exercise", "Read"]
     
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(habits, id: \.self) {
-                    habit in Text(habit)
-                }
+        ZStack {
+            LinearGradient(
+                gradient: Gradient(colors: [.blue, .purple]),
+                                startPoint: .topLeading,
+                                endPoint: .bottomTrailing
+            ).ignoresSafeArea()
+
+            VStack(spacing: 20) {
+                Text("Habit Tracker")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                    .shadow(radius: 5)
+
+                Text("Stay consistent. Stay motivated.")
+                    .font(.headline)
+                    .foregroundColor(.white.opacity(0.8))
             }
-            .navigationTitle("My Habits")
         }
     }
 }
